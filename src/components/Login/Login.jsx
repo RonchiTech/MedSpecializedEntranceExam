4242cfe8ec3acf5b75fd1bac6d9c74a047e40b6c
ronchitech <ronchibusiness@gmail.com>
2021-03-17 17:45:33 +0800

    added validation

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,8 +3,35 @@ import './Login.scss';
 import Email from '../../assets/icons/message.svg';
 import Password from '../../assets/icons/lock.svg';
 import TogglePassword from '../../assets/icons/view.svg';
+import Google from '../../assets/icons/google.svg';
+import MEM from '../../assets/icons/mem.png';
 const Login = () => {
   const [passwordToggle, setPasswordToggle] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const signIn = () => {
+    const pattern = new RegExp(
+      /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+    );
+    if (!email && !password) {
+      //   alert('Invalid Email or Password');
+      setError('Email or Password must not be empty');
+      return;
+    } else if (!pattern.test(email) && password.length < 5) {
+      setError('Invalid Email or Password');
+      return;
+    } else if (password.length < 5) {
+      setError('Password must have 5 or more characters');
+      return;
+    } else if (!pattern.test(email)) {
+      setError('Invalid Email');
+    } else {
+      setError(null);
+    }
+  };
+
   return (
     <div className="login-card">
       <p className="login-card__welcome">
@@ -13,13 +40,22 @@ const Login = () => {
       <p className="login-card__system-name">Learning Management System</p>
       <div className="login-card__input">
         <img className="left-image" src={Email} alt="Email" />
-        <input type="text" placeholder="Email Address" />
+        <input
+          type="email"
+          placeholder="Email Address"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
       </div>
       <div className="login-card__input">
         <img className="left-image" src={Password} alt="Password" />
         <input
           type={passwordToggle ? 'input' : 'password'}
           placeholder="Password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
         />
         <img
           className="right-image"
@@ -28,9 +64,20 @@ const Login = () => {
           onClick={() => setPasswordToggle((currValue) => !currValue)}
         />
       </div>
-      <button>Sign In</button>
+      <p style={{ color: 'red', marginBotton: '25px' }}>
+        {error ? error : null}
+      </p>
+      <button onClick={() => signIn()}>Sign In</button>
       <p className="login-card__forgot-password">Forgot your password?</p>
       <hr />
+      <div className="login-card__alternate-login">
+        <img src={Google} alt="Google" />
+        <span>Sign in with Gmail</span>
+      </div>
+      <div className="login-card__alternate-login">
+        <img src={MEM} alt="MEM" />
+        <span>Sign in with MEM</span>
+      </div>
     </div>
   );
 };
