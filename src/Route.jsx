33454fd605dc2f1658c4caedab58f33bd277ef45
ronchitech <ronchibusiness@gmail.com>
2021-03-18 19:18:27 +0800

    reroute if path not found

--- src/Route.jsx
+++ src/Route.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import './App.scss';
 import { connect } from 'react-redux';
 import * as action from './store/actions/';
-
+import { Route as Routee, Redirect,  } from 'react-router-dom';
 import Main from './components/Main/Main';
 import Dashboard from './components/Dashboard/Dashboard';
 const Route = ({ isAuth, onLogIn }) => {
@@ -10,10 +10,14 @@ const Route = ({ isAuth, onLogIn }) => {
     onLogIn();
   }, [onLogIn]);
 
-  let display = <Main />;
+  let display = (
+    <>
+      <Routee path="/" component={Main} />
+      <Redirect from="*" to="/" />
+    </>
+  );
   if (isAuth) {
     display = <Dashboard />;
-
   }
 
   return display;
